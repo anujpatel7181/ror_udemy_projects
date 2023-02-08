46d1db4fad47f73ef3a2b8b9dd5d7548abbd1e45
Anuj Patel <anuj@5edev.com>
2023-02-08 18:30:33 +0530

    8/2/23

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :articles
   # Defines the root path route ("/")
   root "articles#index"
+  get 'signup', to: 'users#new'
 end
